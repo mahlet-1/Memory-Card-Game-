18e877aa6fa468d98d470f101f613039c36755ea
mahlet-1 <mahichanie126@gmail.com>
2026-08-20 00:11:42 +0300

    fix: fixed header section styling and added a logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,22 @@
 import '../Styles/Header.css'
+import PokemonLogo from '../assets/PokemonLogo 1.png'
 
 export default function Header({ score, bestScore, onPlayAgain, onRestart}) {
   return (
-    <header className="header-container">
-      <h1 className="game-title">Pokmemo</h1>
+    <header className="header-box">
+      <div className="header-left">
+        <img src={PokemonLogo} alt="Pokmemo Logo" className="game-logo" />
+        <h1 className="game-title">Pokmemo</h1>
+      </div>
+
       <div className="score-board">
         <p>Score: <span>{score}</span></p>
         <p>Best Score: <span>{bestScore}</span></p>
       </div>
-        <div className="header-actions">
-        <button className="header-btn" onClick={onPlayAgain}>Restart</button>
-        <button className="header-btn" onClick={onRestart}>Quit</button>
+
+      <div className="header-actions">
+        <button className="header-btn " onClick={onPlayAgain}>Restart</button>
+        <button className="header-btn " onClick={onRestart}>Quit</button>
       </div>
     </header>
   );
