e90a9df677d8f9773cf3d9cfc8b08a4036376f82
mahlet-1 <mahichanie126@gmail.com>
2026-08-19 22:09:44 +0300

    added a main menu button in the modals

--- src/Components/GameOverModal.jsx
+++ src/Components/GameOverModal.jsx
@@ -1,11 +1,12 @@
 import '../Styles/GameOverModal.css'
-export default function GameOverModal({ onPlayAgain }) {
+export default function GameOverModal({ onPlayAgain, OnRestart }) {
   return (
     <div className="over-modal-overlay">
       <div className="over-modal-content">
         <h2>GAME OVER </h2>
         <p>Oops! You already clicked that card. Better luck on your next try!</p>
         <button className="game-over-btn" onClick={onPlayAgain}>TRY AGAIN</button>
+        <button className="menu-btn" onClick={OnRestart}> MAIN MENU</button>
       </div>
     </div>
   );
--- src/Components/WinModal.jsx
+++ src/Components/WinModal.jsx
@@ -1,11 +1,12 @@
 import '../Styles/WinModal.css'
-export default function WinModal({ onPlayAgain}) {
+export default function WinModal({ onPlayAgain, OnRestart}) {
   return (
     <div className="win-modal-overlay">
       <div className="win-modal-content">
         <h2>VICTORY!</h2>
         <p>Congrats! You successfully caught all the Pokemon Cards.</p>
         <button className="win-btn" onClick={onPlayAgain}>PLAY AGAIN</button>
+        <button className="menu-btn" onClick={OnRestart}> MAIN MENU</button>
       </div>
     </div>
   );
