cfaac38088664c6952b2c1ec0e9e6bfb232cc3f2
mahlet-1 <mahichanie126@gmail.com>
2026-08-19 21:13:11 +0300

    added win modal styling

--- src/Components/GameOverModal.jsx
+++ src/Components/GameOverModal.jsx
@@ -1,8 +1,8 @@
 import '../Styles/GameOverModal.css'
 export default function GameOverModal({ onPlayAgain }) {
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
+    <div className="over-modal-overlay">
+      <div className="over-modal-content">
         <h2>GAME OVER </h2>
         <p>Oops! You already clicked that card. Better luck on your next try!</p>
         <button className = "game-over-btn" onClick={onPlayAgain}>TRY AGAIN</button>
--- src/Components/WinModal.jsx
+++ src/Components/WinModal.jsx
@@ -1,10 +1,11 @@
+import '../Styles/WinModal.css'
 export default function WinModal({ onPlayAgain }) {
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <h2>Victory!</h2>
-        <p>Congrats! You successfully caught all the Pokemon.</p>
-        <button onClick={onPlayAgain}>Play Again</button>
+    <div className="win-modal-overlay">
+      <div className="win-modal-content">
+        <h2>VICTORY!</h2>
+        <p>Congrats! You successfully caught all the Pokemon Cards.</p>
+        <button className="win-btn" onClick={onPlayAgain}>PLAY AGAIN</button>
       </div>
     </div>
   );
