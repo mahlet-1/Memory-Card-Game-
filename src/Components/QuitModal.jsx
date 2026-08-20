54f3abb0efc8c445834cdf96ad7b5aca825eb65b
mahlet-1 <mahichanie126@gmail.com>
2026-08-20 22:36:01 +0300

    added quit modal styling

--- src/Components/QuitModal.jsx
+++ src/Components/QuitModal.jsx
@@ -1,3 +1,4 @@
+import '../Styles/QuitModal.css';
 export default function QuitModal({ onQuit, onCancelQuit }) {
   return (
     <div className="quit-modal-overlay">
@@ -5,8 +6,8 @@ export default function QuitModal({ onQuit, onCancelQuit }) {
         <h2>Are you sure you want to quit?</h2>
         <p>Your current game progress will be lost!</p>
         <div className="quit-btn">
-          <button onClick={onQuit}>Yes</button>
-          <button onClick={onCancelQuit}>No</button>
+          <button className='yes-btn' onClick={onQuit}>Yes</button>
+          <button className='no-Btn' onClick={onCancelQuit}>No</button>
         </div>
       </div>
     </div>
