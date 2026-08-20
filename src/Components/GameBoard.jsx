d4bd801dad813dfd72a465be0baa6de6dfebf24e
mahlet-1 <mahichanie126@gmail.com>
2026-08-20 18:44:29 +0300

    added new game board content and updated the styling

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,15 +1,25 @@
 import '../Styles/GameBoard.css'
 import Card from './Card';
+
 export default function GameBoard({ cards, onCardClick }) {
   return (
-    <div className="game-board">
-      {cards.map((pokemon) => (
-        <Card 
-          key={pokemon.id} 
-          pokemon={pokemon} 
-          onCardClick={onCardClick} 
-        />
-      ))}
+    <div className="game-container">
+      <div className="top-text">
+        <p>Status: Playing</p>
+      </div>
+
+      <div className="game-board">
+        {cards.map((pokemon) => (
+          <Card
+            key={pokemon.id}
+            pokemon={pokemon}
+            onCardClick={onCardClick}
+          />
+        ))}
+      </div>
+      <div className="bottom-text">
+        <p> CAN YOU BEAT YOUR BEST SCORE? </p>
+      </div>
     </div>
   );
 }
